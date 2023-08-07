40674e9df29c5001cc78a94122cad417d629e06d
Linconl <67241771+LinconlG@users.noreply.github.com>
2023-08-07 15:55:59 -0300

    Create Treino.cs

---
+++ Models/Treino.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinoSport.Models {
+    public class Treino {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public List<Usuario> Alunos { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public int CodigoCriador { get; set; }
+        public List<DayOfWeek> DatasTreinos { get; set; }
+        public DateTime DataVencimento { get; set; }
+        //imagem
+    }
+}
