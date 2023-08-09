b8a3e543e6b65cdd347e99dacaeeb4daac74608e
Linconl <67241771+LinconlG@users.noreply.github.com>
2023-08-09 11:47:48 -0300

    criado a pagina de cadastro

--- XMLPages/CadastroPage.xaml.cs
+++ XMLPages/CadastroPage.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui.Devices;
+using TreinoSport.Models;
+
 namespace TreinoSport.XMLPages;
 
 public partial class CadastroPage : ContentPage
@@ -5,5 +8,42 @@ public partial class CadastroPage : ContentPage
 	public CadastroPage()
 	{
 		InitializeComponent();
+		CadastrarBtn.Clicked += ClickCadastrarBtn;
 	}
+
+	private void ClickCadastrarBtn(object sender, EventArgs e) {
+		
+		if (CheckCampos()) {
+			return;
+		}
+		var usuario = new Usuario();
+		usuario.Nome = nomeCompletoEntry.Text;
+        usuario.Email = emailEntry.Text;
+        usuario.Senha = senhaEntry.Text;
+    }
+	private bool CheckCampos() {
+		var flag = false;
+		if (nomeCompletoEntry.Text == String.Empty) {
+			avisoNomeLabel.IsVisible = true;
+			flag = true;
+		}
+		else {
+            avisoNomeLabel.IsVisible = false;
+        }
+        if (emailEntry.Text == String.Empty) {
+            avisoEmailLabel.IsVisible = true;
+            flag = true;
+        }
+        else {
+            avisoEmailLabel.IsVisible = false;
+        }
+        if (senhaEntry.Text == String.Empty) {
+            avisoSenhaLabel.IsVisible = true;
+            flag = true;
+        }
+        else {
+            avisoSenhaLabel.IsVisible = false;
+        }
+        return flag;
+    }
 }
