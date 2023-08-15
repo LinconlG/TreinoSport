e4574ef219f736aa47b5300b91169561ccb84abc
Linconl <67241771+LinconlG@users.noreply.github.com>
2023-08-15 10:44:05 -0300

    paginaInicial em andamento

---
+++ ViewModels/TreinoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinoSport.Contexts;
+using TreinoSport.Models;
+
+namespace TreinoSport.ViewModels {
+    public partial class TreinoViewModel : ObservableObject {
+
+        private TreinoContext _treinoContext;
+        public ObservableCollection<Treino> Treinos { get; set; }
+        [ObservableProperty]
+        private Treino treino;
+        [ObservableProperty]
+        private bool _isBusy;
+
+        public TreinoViewModel() {
+            _treinoContext = new();
+            Treinos = new();
+        }
+
+        [ICommand]
+        private async Task GetTreinosAluno() {
+            try {
+                IsBusy = true;
+                Treinos.Clear();
+                var lista = await _treinoContext.GetTreinosAluno(Preferences.Get("codigoUsuario", 0));
+                foreach (var item in lista) {
+                    Treinos.Add(item);
+                }
+            }
+            catch (Exception e) {
+                throw new Exception(e.Message);
+            }
+            finally {
+                IsBusy = false;
+            }
+
+        }
+
+        public void OnAppearing() {
+            IsBusy = true;
+        }
+    }
+}
